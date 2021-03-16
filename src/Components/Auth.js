fbf7298aa1647783430bbde33743a3b0d69d779c
Eduard Carrerars <ecarreras@gisce.net>
2021-03-16 13:51:30 +0100

    Store token to a cookie (24h)

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,18 +1,27 @@
 import React, { useContext, createContext, useState } from "react";
+import Cookies from 'universal-cookie';
 import axios from "axios";
+
+const cookies = new Cookies();
 
 
 const Auth = {
-  isAuthenticated: false,
-  token: null,
+  isAuthenticated: cookies.get('token') ? true : false,
+  token: cookies.get('token'),
   async signin(username, password) {
+    let token = Auth.token;
     try {
-      const result = await axios.get('http://10.246.0.198:8067/token', {
-        auth: { username, password }
-      })
-      console.log("Login successful", result.data.token);
+      if (!Auth.token) {
+        const result = await axios.get('http://10.246.0.198:8067/token', {
+          auth: { username, password }
+        })
+        console.log("Login successful", result.data.token);
+        let token = result.data.token;
+        console.log('Storing token cookie', token);
+        cookies.set('token', token, { maxAge: 3600 * 24 });
+      }
       Auth.isAuthenticated = true;
-      Auth.token = result.data.token;
+      Auth.token = token;
     } catch (exc) {
       Auth.signout();
     }
@@ -43,7 +52,7 @@ export function useAuth() {
 
 
 function useProvideAuth() {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(cookies.get('token'));
 
   const signin = async (username, password) => {
     await Auth.signin(username, password);
