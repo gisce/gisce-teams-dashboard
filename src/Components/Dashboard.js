d3a379435ab356170ac7f763dc14e910ecdecb60
Eduard Carrerars <ecarreras@gisce.net>
2021-03-19 14:04:37 +0100

    Fetch team tasks async

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ApiClient from "../Services/ApiClient";
-import _ from "underscore";
+import _ from "lodash";
 import { Grommet, Group, Update, Columns } from "grommet-icons";
 import { useHistory } from "react-router-dom";
 import {
@@ -19,15 +19,79 @@ export const graphColors = {
   "Archive": "graph-4"
 }
 
+
+const TeamCard = ({ id, name, members }) => {
+  const [loading, setLoading] = useState(true);
+  const [tasks, setTasks] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await ApiClient.get(`/ProjectTask?filter=[('team_id','=',${id}),('stage_id.name','!=','Archive')]&schema=stage_id.name&limit=1000`);
+      setTasks(result.data.items);
+      setLoading(false);
+    };
+    fetch();
+  }, [id]);
+
+  const groupedTasks = _.groupBy(tasks, t => {
+    const stage = t.stage_id ?? { name: "undefined" };
+    return stage.name;
+  })
+  const meterValues = _.entries(groupedTasks).map(i => { return { label: i[0], value: i[1].length, color: graphColors[i[0]] } });
+
+  return (
+    <Card>
+      <CardHeader align="center" direction="row" flex={false} justify="center" gap="medium" pad="small" fill="horizontal">
+        <Heading textAlign="center" size="small" level="2">
+          {name}
+        </Heading>
+      </CardHeader>
+      <CardBody pad="small" justify="start" direction="column" align="center">
+      {loading && <Box animation="rotateRight" pad="medium">
+          <Update size="xlarge" color="brand"/>
+        </Box>
+}
+{!loading && <>
+        <Meter values={meterValues} type="circle" size="small" thickness="medium" round />
+        <Box align="start" justify="center" gap="small" pad="xsmall">
+          {meterValues.map(v => (
+            <Box align="center" justify="center" direction="row" pad="xsmall" gap="small">
+              <Grommet color={graphColors[v.label]} size="medium" />
+              <Paragraph fill={false} margin="none">
+                {`${v.label}: ${v.value}`}
+              </Paragraph>
+            </Box>
+          )
+          )}
+        </Box>
+        </>
+}
+      </CardBody>
+      <CardFooter align="center" direction="row" flex={false} justify="center" gap="medium" pad="small">
+        <Box align="center" justify="center" direction="column" gap="small">
+          <Button label="Go to board" icon={<Columns />} primary onClick={() => history.push(`/team/${id}`)} />
+          <Box direction="row" fill="horizontal" gap="small" justify="center">
+            <Group />
+            <Text>{members.length} members</Text>
+          </Box>
+        </Box>
+      </CardFooter>
+    </Card>
+  )
+}
+
+
 const Dashboard = ({ props }) => {
   const [teams, setTeams] = useState([]);
+  const [groupedTasks, setGroupedTasks] = useState({});
   const [loading, setLoading] = useState(true);
   const history = useHistory();
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const result = await ApiClient.get("/ProjectTeam?schema=name,task_ids.stage_id.name,member_ids");
+        const result = await ApiClient.get("/ProjectTeam?schema=name,member_ids");
         setTeams(result.data.items);
       }
       catch (exc) {
@@ -41,43 +105,7 @@ const Dashboard = ({ props }) => {
 
   }, [])
 
-  const teamCards = teams.map(team => {
-    const groupedTasks = _.groupBy(team.task_ids, t => {
-      const stage = t.stage_id ?? { name: "undefined" };
-      return stage.name;
-    })
-    const meterValues = _.pairs(groupedTasks).map(i => { console.log(i); return { label: i[0], value: i[1].length, color: graphColors[i[0]] } });
-    return (<Card key={team.id}>
-      <CardHeader align="center" direction="row" flex={false} justify="center" gap="medium" pad="small" fill="horizontal">
-        <Heading textAlign="center" size="small" level="2">
-          {team.name}
-        </Heading>
-      </CardHeader>
-      <CardBody pad="small" justify="start" direction="column" align="center">
-        <Meter values={meterValues} type="circle" size="small" thickness="medium" round />
-        <Box align="start" justify="center" gap="small" pad="xsmall">
-          {meterValues.map(v => (
-            <Box align="center" justify="center" direction="row" pad="xsmall" gap="small">
-              <Grommet color={graphColors[v.label]} size="medium" />
-              <Paragraph fill={false} margin="none">
-                {`${v.label}: ${v.value}`}
-              </Paragraph>
-            </Box>
-          )
-          )}
-        </Box>
-      </CardBody>
-      <CardFooter align="center" direction="row" flex={false} justify="center" gap="medium" pad="small">
-        <Box align="center" justify="center" direction="column" gap="small">
-          <Button label="Go to board" icon={<Columns />} primary onClick={() => history.push(`/team/${team.id}`)} />
-          <Box direction="row" fill="horizontal" gap="small" justify="center">
-            <Group />
-            <Text>{team.member_ids.length} members</Text>
-          </Box>
-        </Box>
-      </CardFooter>
-    </Card>)
-  })
+  const teamCards = teams.map(team => <TeamCard id={team.id} name={team.name} members={team.member_ids} />);
   return (
     <Box fill="vertical" overflow="auto" align="center" flex="grow" pad="medium">
       <Box align="end" justify="center" fill="horizontal" pad="medium">
